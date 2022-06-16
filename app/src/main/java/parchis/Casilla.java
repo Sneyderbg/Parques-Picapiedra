27bc7e433522d2a8cc5a54ceeca3f098de73d22e
Sneyderbg <sneyder.buitrago@udea.edu.co>
2022-06-16 15:04:49 -0500

    Confirmar

--- app/src/main/java/parchis/Casilla.java
+++ app/src/main/java/parchis/Casilla.java
@@ -1,9 +1,24 @@
 package parchis;
 
+import java.util.ArrayList;
+
+import javafx.scene.paint.Color;
 import parchis.Message.Type;
 
+/**
+ * Clase que representa las casillas especiales del parqués, siendo éstas:
+ * Cárcel, salida, seguro, entrada.
+ */
 public class Casilla {
 
+    /**
+     * Enumerado para describir el tipo de {@link Casilla}.<br>
+     * 
+     * - CARCEL.<br>
+     * - SALIDA.<br>
+     * - SEGURO <br>
+     * - ENTRADA.
+     */
     public enum TipoCasilla {
         CARCEL,
         SALIDA,
@@ -11,68 +26,125 @@ public class Casilla {
         ENTRADA,
     }
 
+    /**
+     * Máximo número de fichas que caben en una casilla.
+     */
     private static final int MAX_FICHAS = 4;
 
-    private int numeroCasilla;
+    /**
+     * Identificador de casilla;
+     */
+    private int idCasilla;
 
+    /**
+     * Atributo que describe el tipo de casilla.
+     */
     private TipoCasilla tipoCasilla;
 
+    /**
+     * Color de la casilla. Este se asigna automáticamente según el
+     * {@link jugadorPadre}.
+     */
+    private Color color;
+
+    /**
+     * Jugador a la que esta casilla pertenece.
+     */
     private Jugador jugadorPadre;
 
-    private Ficha fichas[];
+    /**
+     * Vector que contiene las fichas que actualmente están en esta {@link Casilla}
+     */
+    private ArrayList<Ficha> fichas;
 
-    public Casilla(Jugador jugadorPadre, int numeroCasilla, TipoCasilla tipo) {
+    /**
+     * Constructor. Crea una casilla con los parámetros dados.
+     * 
+     * @param jugadorPadre Jugador al que pertenece esta {@linkCasilla}.
+     * @param IdCasilla    Entero que identifica esta {@link Casilla}.
+     * @param tipo         Tipo de {@link Casilla}. Veáse {@link TipoCasilla}.
+     */
+    public Casilla(Jugador jugadorPadre, int IdCasilla, TipoCasilla tipo) {
 
-        this.numeroCasilla = numeroCasilla;
+        this.idCasilla = IdCasilla;
         this.tipoCasilla = tipo;
-        this.fichas = new Ficha[MAX_FICHAS];
+        this.fichas = new ArrayList<Ficha>();
+        this.color = jugadorPadre.getColor();
 
     }
 
+    /**
+     * Inserta una ficha en el vector {@link fichas} siempre que no sobrepase el
+     * máximo número de fichas {@link MAX_FICHAS}.
+     * 
+     * @param ficha {@link Ficha} a insertar en esta {@link Casilla}.
+     * @return Un mensaje describiendo el proceso hecho. Veáse {@link Message}.
+     */
     public Message insertarFicha(Ficha ficha) {
 
-        int i = 0;
-        while (i < MAX_FICHAS && fichas[i] != null) {
-            i++;
-        }
-
-        if (i == MAX_FICHAS) {
+        if (fichas.size() == MAX_FICHAS) {
             return new Message(Type.ERROR, "No es posible insertar más de %d fichas".formatted(MAX_FICHAS));
         }
 
-        fichas[i] = ficha;
-        return new Message(Type.SUCCESS, "Ficha insertada en la posición %d".formatted(i));
+        fichas.add(ficha);
+        return new Message(Type.SUCCESS, "Ficha insertada en la posición %d".formatted(fichas.size()));
 
     }
 
+    /**
+     * Elimina una ficha del vector {@link fichas} en caso de que ésta esté
+     * presente, de lo contrario no hace nada.
+     * 
+     * @param ficha {@link Ficha} a eliminar del vector {@link fichas}.
+     * @return Un mensaje describiendo el proceso hecho. Veáse {@link Message}.
+     */
     public Message removerFicha(Ficha ficha) {
 
-        int i = 0;
-        while (i < MAX_FICHAS && fichas[i] != ficha) {
-            i++;
-        }
+        int i = fichas.indexOf(ficha);
 
-        if (i == MAX_FICHAS) {
+        if (i == -1) {
             return new Message(Type.ERROR, "La ficha no existe.");
         }
 
-        fichas[i] = null;
+        fichas.remove(i);
         return new Message(Type.SUCCESS, "ficha eliminada de la posición %d".formatted(i));
 
     }
 
-    public int getNumeroCasilla() {
-        return this.numeroCasilla;
+    /**
+     * Retorna el identificador de esta {@link Casilla}.
+     * 
+     * @return {@link #idCasilla}.
+     */
+    public int getIdCasilla() {
+        return this.idCasilla;
     }
 
+    /**
+     * Retorna el tipo de casilla de esta {@link Casilla}.
+     * 
+     * @return {@link #tipoCasilla}.
+     */
     public TipoCasilla getTipoCasilla() {
-        return tipoCasilla;
+        return this.tipoCasilla;
     }
 
+    /**
+     * Retorna el {@link Jugador} al que esta {@link Casilla} pertenece.
+     * 
+     * @return {@link #jugadorPadre}.
+     */
     public Jugador getJugadorPadre() {
-        return jugadorPadre;
+        return this.jugadorPadre;
     }
 
-    
-    
+    /**
+     * Retorna el color de esta Casilla.
+     * 
+     * @return {@link #color}.
+     */
+    public Color getColor() {
+        return color;
+    }
+
 }
--- app/src/main/java/parchis/Jugador.java
+++ app/src/main/java/parchis/Jugador.java
@@ -1,76 +1,110 @@
 package parchis;
 
-import java.awt.Color;
+import java.util.ArrayList;
+
+import javafx.scene.paint.Color;
 
 /**
  * 
  */
 public class Jugador {
 
+    /**
+     * String que describe el nombre de este Jugador.
+     */
     private String nombre;
 
-    private int numJugador;
+    /**
+     * Entero con el cuál se identifica este Jugador.
+     */
+    private int idJugador;
 
+    /**
+     * Color con el cuál se mostrará las fichas y casillas de este Jugador.
+     */
     private Color color;
 
-    private boolean vivo;
-
+    /**
+     * Número de fichas que tiene este Jugador.
+     */
     private int numFichas;
 
-    private Ficha fichas[];
+    /**
+     * Vector que contiene las fichas pertenecientes a este Jugador.
+     */
+    private ArrayList<Ficha> fichas;
 
+    /**
+     * Casilla que representa la cárcel (en donde las fichas están inicialmente), de
+     * este Jugador.
+     */
     private Casilla carcel;
 
+    /**
+     * Casilla que representa el cielo (en donde las fichas deben de llegar), de
+     * este Jugador.
+     */
     private Casilla cielo;
 
     /**
+     * Constructor. Crea un jugador con los parámetros dados.
      * 
-     * @param nombre
-     * @param numJugador
-     * @param numFichas
-     * @param color
+     * @param nombre    Nombre del Jugador.
+     * @param idJugador Identificador del Jugador.
+     * @param numFichas Número de fichas que tendrá el Jugador.
+     * @param color     {@link Color} del Jugador, de sus Fichas y sus Casillas.
      */
-    public Jugador(String nombre, int numJugador, Color color, int numFichas) {
+    public Jugador(String nombre, int idJugador, Color color, int numFichas) {
 
         assert (numFichas >= 2 && numFichas <= 4) : "El número de fichas debe estar entre 2 y 4 incluidos.";
 
         this.nombre = nombre;
-        this.numJugador = numJugador;
+        this.idJugador = idJugador;
         this.numFichas = numFichas;
         this.color = color;
-        this.vivo = true;
         crearFichas();
 
     }
 
+    /**
+     * Crea <i>n</i> fichas para este Jugador, donde <i>n</i> está descrito en
+     * {@link #numFichas}.
+     */
     public void crearFichas() {
 
-        fichas = new Ficha[numFichas];
+        fichas = new ArrayList<Ficha>();
 
         for (int i = 0; i < numFichas; i++) {
-            fichas[i] = new Ficha(this, this.carcel);
+            fichas.add(new Ficha(this, this.carcel));
         }
 
     }
 
+    /**
+     * Retorna el nombre de este Jugador.
+     * 
+     * @return {@link #nombre}.
+     */
     public String getNombre() {
         return nombre;
     }
 
-    public int getNumJugador() {
-        return numJugador;
+    /**
+     * Retorna el identificador de este Jugador.
+     * 
+     * @return {@link #idJugador}.
+     */
+    public int getIdJugador() {
+        return idJugador;
     }
 
+    /**
+     * Retorna el color de este Jugador.
+     * 
+     * @return {@link #color}.
+     */
     public Color getColor() {
         return color;
     }
 
-    public boolean estaVivo() {
-        return vivo;
-    }
-
-    public void setEstaVivo(boolean vivo) {
-        this.vivo = vivo;
-    }
-    
 }
