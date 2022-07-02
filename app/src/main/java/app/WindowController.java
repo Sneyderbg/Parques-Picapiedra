2d4b39c7fa94ab29cf9f4ea9d2f4112af4d47058
Sneyderbg <sneyder.buitrago@udea.edu.co>
2022-07-01 23:34:33 -0500

    'parchis6.png' añadido y modificado para mejor visualización.

---
+++ app/src/main/java/app/WindowController.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class WindowController implements Initializable{
+    
+    private ParchisDrawingCanvas parchisDrawingCanvas;
+    
+    @FXML
+    private AnchorPane drawingPane;
+    
+    @FXML
+    private AnchorPane bottomPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        parchisDrawingCanvas = new ParchisDrawingCanvas();
+        parchisDrawingCanvas.widthProperty().bind(drawingPane.widthProperty().subtract(2));
+        parchisDrawingCanvas.heightProperty().bind(drawingPane.heightProperty().subtract(2));
+        drawingPane.getChildren().add(parchisDrawingCanvas);
+        
+        parchisDrawingCanvas.nuevoTablero();
+        
+    }
+    
+}
